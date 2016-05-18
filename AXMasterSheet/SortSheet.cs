c9ff5c55bfbb26d2600952c46e7caa220c78174d
Yuta Kobayashi <kobayashi@lotusbc.co.jp>
2016-05-18 11:17:13 +0900

    create copy cell method

--- AXMasterSheet/SortSheet.cs
+++ AXMasterSheet/SortSheet.cs
@@ -10,78 +10,115 @@ namespace AXMasterSheet
 {
     class SortSheet
     {
-        static public void Sort(string strFileName)
+        static public int Sort(string strFileName)
         {
-            var wb = new XLWorkbook(strFileName);
-            var wsList = wb.Worksheet("List");
+            Console.WriteLine("Read .xlsx file.");
+            var wb = new XLWorkbook();
+            try
+            {
+                wb = new XLWorkbook(strFileName);
+            }
+            catch
+            {
+                Console.WriteLine("Reading xlsx file was failed.");
+                return 1;
+            }
+
+            const string strListSheetName = "List";
+            const string strMasterSheetName = "Master";
+
+            var wsList = wb.Worksheet(strListSheetName);
            
             try
             {
-                wb.Worksheet("Master").Delete();
-
+                wb.Worksheet(strMasterSheetName).Delete();
+                Console.WriteLine("Master sheet was deleted.");
             }
             catch
             {
             }
 
-            var wsMaster = wb.Worksheets.Add("Master");
-
+            var wsMaster = wb.Worksheets.Add(strMasterSheetName);
+            Console.WriteLine("New master sheet was added.");
 
             int intListMaxRow = wsList.RowsUsed().Count();
 
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
 
-            dt.Columns.Add("Numb");
-            dt.Columns.Add("Tab1");
-            dt.Columns.Add("Tab2");
-            dt.Columns.Add("Tab3");
-            dt.Columns.Add("Tab4");
-            dt.Columns.Add("Item");
-            dt.Columns.Add("Use");
+            dt.Columns.Add("Numb"); //[0]
+            dt.Columns.Add("Tab1"); //[1]
+            dt.Columns.Add("Tab2"); //[2]
+            dt.Columns.Add("Tab3"); //[3]
+            dt.Columns.Add("Tab4"); //[4]
+            dt.Columns.Add("Item"); //[5]
+            dt.Columns.Add("Use");  //[6]
 
-            string strCheckTab1 = "";
-            string strCheckTab2 = "";
-            string strCheckTab3 = "";
-            string strCheckTab4 = "";
-            string strCheckItem = "";
-            string strCheckUse = "";
+            string strCopyValue1 = "";
+            string strCopyValue2 = "";
+            string strCopyValue3 = "";
+            string strCopyValue4 = "";
+
 
             for (int i = 2; i <= intListMaxRow; i++)
             {
                 DataRow dr = dt.NewRow();
 
-                dr["Numb"] = i - 1;
+                dr[0] = i - 1;
 
-                if (wsList.Cell(i,2).Value.ToString() == "")
+                string strColumnValue1 = wsList.Cell(i, 2).Value.ToString();
+                string strColumnValue2 = wsList.Cell(i, 3).Value.ToString();
+                string strColumnValue3 = wsList.Cell(i, 4).Value.ToString();
+                string strColumnValue4 = wsList.Cell(i, 5).Value.ToString();
+
+                if (strColumnValue1 != "")
                 {
-                    dr["Tab1"] = strCheckTab1;                    
-                }
-                else
-                { 
-                    dr["Tab1"] = wsList.Cell(i, 2).Value.ToString();
-                    strCheckTab1 = wsList.Cell(i, 2).Value.ToString();
+                    strCopyValue1 = strColumnValue1;
                 }
 
-                dr["Tab2"] = wsList.Cell(i, 3).Value;
-                dr["Tab3"] = wsList.Cell(i, 4).Value;
-                dr["Tab4"] = wsList.Cell(i, 5).Value;
+                CopyCell(strColumnValue2, ref strCopyValue2, strColumnValue1, strCopyValue1);
+                CopyCell(strColumnValue3, ref strCopyValue3, strColumnValue2, strCopyValue2);
+                CopyCell(strColumnValue4, ref strCopyValue4, strColumnValue3, strCopyValue3);
+
+                dr[1] = strCopyValue1;
+                dr[2] = strCopyValue2;
+                dr[3] = strCopyValue3;
+                dr[4] = strCopyValue4;
+
                 dr["Item"] = wsList.Cell(i, 6).Value;
-                dr["Use"] = wsList.Cell(i, 7).Value;
+                dr["Use"]  = wsList.Cell(i, 7).Value;
 
                 dt.Rows.Add(dr);
-
-
             }
 
             for (int i = 1; i <= dt.Rows.Count; i++)
             {
-                wsMaster.Cell(i, 1).Value = dt.Rows[i - 1]["Tab1"];
-                wsMaster.Cell(i, 2).Value = dt.Rows[i - 1]["Tab2"];
-
+                wsMaster.Cell(i, 1).Value = dt.Rows[i - 1][0];
+                wsMaster.Cell(i, 2).Value = dt.Rows[i - 1][1];
+                wsMaster.Cell(i, 3).Value = dt.Rows[i - 1][2];
+                wsMaster.Cell(i, 4).Value = dt.Rows[i - 1][3];
+                wsMaster.Cell(i, 5).Value = dt.Rows[i - 1][4];
+                wsMaster.Cell(i, 6).Value = dt.Rows[i - 1][5];
+                wsMaster.Cell(i, 7).Value = dt.Rows[i - 1][6];
             }
 
             wb.SaveAs(strFileName);
+
+            return 0;
         }
+
+        static private void CopyCell(string strColumnValue,ref string strCopyValue, string strCheckColumnValue, string strCheckCopyValue)
+        {
+            if (strCheckColumnValue == strCheckCopyValue)
+            {
+                strCopyValue = "";
+            }
+
+            if (strColumnValue != "")
+            {
+                strCopyValue = strColumnValue;
+            }
+        }
+
     }
 }
